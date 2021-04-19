0e06db8c4ac36f410927ace0b9078d94137fe1eb
Loren Burkholder <computersemiexpert@outlook.com>
2021-04-19 16:16:54 -0400

    Updated .ts file

--- barrel_en_US.ts
+++ barrel_en_US.ts
@@ -1,13 +1,15 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="barrel_en_US">
+<TS version="2.1" language="en_US">
 <context>
     <name>QObject</name>
     <message>
+        <location filename="main.cpp" line="23"/>
         <source>File to read data from. If this is not specified, barrel will start in program input mode.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="main.cpp" line="52"/>
         <source>Could not open file %1!</source>
         <translation type="unfinished"></translation>
     </message>
